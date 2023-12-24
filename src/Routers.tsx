a0534528ec902376cfc2828db20d2f0fcc3710c0
shimizu <noon3m.op.sh@gmail.com>
2023-12-25 04:30:09 +0900

    [add]ログイン画面に必要なコンポーネント作成

--- src/Routers.tsx
+++ src/Routers.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 
-const Routers = () => {
+export const Routers = () => {
   return (
     <BrowserRouter>
       <Routes>
