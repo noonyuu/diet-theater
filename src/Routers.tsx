efb956e5db14832059959a29d3e5bdddef95316c
yutaro shimizu <107845401+noonyuu@users.noreply.github.com>
2023-12-27 01:53:17 +0900

    Revert "Feature/navbar"

--- src/Routers.tsx
+++ src/Routers.tsx
@@ -1,22 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
-import { LoginPage } from "./pages/admin/login";
-import { Terms } from "./pages/terms";
-import { Agenda } from "./pages/admin/agenda";
-import { Navbar } from "./component/navbar";
 
-export const Routers = () => {
+const Routers = () => {
   return (
-    <div>
-      <Navbar />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<App />}></Route>
-          <Route path="/login" element={<LoginPage />}></Route>
-          <Route path="/terms" element={<Terms />} />
-          <Route path="/agenda" element={<Agenda />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 };
