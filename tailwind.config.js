c9bae25a4f12469566e170fc04c003d82df6c585
shimizu <noon3m.op.sh@gmail.com>
2024-01-11 13:12:36 +0900

    [update]色を追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,8 @@ module.exports = {
       colors: {
         "main-color": "#A2B0C7",
         "custom-black": "#323232",
+        "chat": "#FDFDFD",
+        "bac-main": "#FBFBFB",
       },
     },
   },
