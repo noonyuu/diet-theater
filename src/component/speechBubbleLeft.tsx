6776086d23ac5a060c1cdb471388a01325e8c30f
shimizu <noon3m.op.sh@gmail.com>
2024-01-29 01:13:28 +0900

    [update]幅を調整

--- src/component/speechBubbleLeft.tsx
+++ src/component/speechBubbleLeft.tsx
@@ -9,7 +9,7 @@ export const BubbleLeft = (props:any) => {
   return (
     <div className="flex pb-5">
       <div className="chat chat-end w-full">
-        <div className="bg-chat chat-bubble max-w-[70vw] tracking-wide text-black shadow-lg">
+        <div className="bg-chat chat-bubble max-w-[80vw] tracking-wide text-black shadow-lg">
           {props.content}
         </div>
       </div>
