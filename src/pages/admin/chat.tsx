a8c1452eb40e25a407d132f0f0a845b447b259d4
shimizu <noon3m.op.sh@gmail.com>
2024-01-29 01:14:32 +0900

    [update]iconをsvgに置き換え。 タブの修正

--- src/pages/admin/chat.tsx
+++ src/pages/admin/chat.tsx
@@ -9,6 +9,7 @@ import { FaSearch } from "react-icons/fa";
 
 ///
 import Data from "../../test/test.json";
+import ListSvg from "../../assets/ListSvg";
 ///
 
 export const Chat = () => {
@@ -18,13 +19,13 @@ export const Chat = () => {
   const [detailId, setDetailId] = useState("");
   const [api, setApi] = useState<Map<string, any>>(new Map());
   const [activeTab, setActiveTab] = useState(1);
-  
+
   useEffect(() => {
     if (!location.state || location.state.detailId == null) {
       navigate("/agenda");
       return;
     }
-    
+
     setDetailId(location.state.detailId);
     // console.log(detailId)
     const fetchData = async () => {
@@ -67,16 +68,16 @@ export const Chat = () => {
     setActiveTab(tabNumber);
   };
 
-    interface SpeechRecord {
-      [key: string]: {
-        speaker: string;
-        speech: string;
-      };
-    }
+  interface SpeechRecord {
+    [key: string]: {
+      speaker: string;
+      speech: string;
+    };
+  }
 
-    const concise: SpeechRecord = Data.all.concise_speechRecords;
-    const child: SpeechRecord = Data.all.chiled_speechRecords;
-    const origin: SpeechRecord = Data.all.original_speechRecords
+  const concise: SpeechRecord = Data.all.concise_speechRecords;
+  const child: SpeechRecord = Data.all.childe_speechRecords;
+  const origin: SpeechRecord = Data.all.original_speechRecords;
 
   const TabContent1 = () => {
     return (
@@ -91,18 +92,30 @@ export const Chat = () => {
               <React.Fragment key={i}>
                 {i % 2 === 0 ? (
                   <div className="flex items-end justify-start">
-                    <div className="">
-                      <img src={icon} alt="logo" className="mx-auto" />
-                      <p>{origin[i]?.speaker}</p>
+                    <div className="w-24 text-center">
+                      <ListSvg
+                        fill="#FF0000"
+                        style={{ margin: "auto" }}
+                        color={"blue"}
+                      />
+                      <p className="pt-1 text-sm md:text-lg">
+                        {origin[i]?.speaker}
+                      </p>
                     </div>
                     <BubbleRight content={formattedSpeech} />
                   </div>
                 ) : (
                   <div className="flex items-end justify-end">
                     <BubbleLeft content={formattedSpeech} />
-                    <div className="text-center">
-                      <img src={icon} alt="logo" className="mx-auto" />
-                      <p>{origin[i]?.speaker}</p>
+                    <div className="w-24 text-center">
+                      <ListSvg
+                        fill="#FF0000"
+                        style={{ margin: "auto" }}
+                        color={"blue"}
+                      />
+                      <p className="pt-1 text-sm md:text-lg">
+                        {origin[i]?.speaker}
+                      </p>
                     </div>
                   </div>
                 )}
@@ -113,7 +126,6 @@ export const Chat = () => {
       </div>
     );
   };
-
 
   const TabContent2 = () => {
     return (
@@ -128,18 +140,30 @@ export const Chat = () => {
               <React.Fragment key={i}>
                 {i % 2 === 0 ? (
                   <div className="flex items-end justify-start">
-                    <div className="">
-                      <img src={icon} alt="logo" className="mx-auto" />
-                      <p>{concise[i]?.speaker}</p>
+                    <div className="w-24 text-center">
+                      <ListSvg
+                        fill="#FF0000"
+                        style={{ margin: "auto" }}
+                        color={"blue"}
+                      />
+                      <p className="pt-1 text-sm md:text-lg">
+                        {origin[i]?.speaker}
+                      </p>
                     </div>
                     <BubbleRight content={formattedSpeech} />
                   </div>
                 ) : (
                   <div className="flex items-end justify-end">
                     <BubbleLeft content={formattedSpeech} />
-                    <div className="text-center">
-                      <img src={icon} alt="logo" className="mx-auto" />
-                      <p>{concise[i]?.speaker}</p>
+                    <div className="w-24 text-center">
+                      <ListSvg
+                        fill="#FF0000"
+                        style={{ margin: "auto" }}
+                        color={"blue"}
+                      />
+                      <p className="pt-1 text-sm md:text-lg">
+                        {origin[i]?.speaker}
+                      </p>
                     </div>
                   </div>
                 )}
@@ -164,18 +188,30 @@ export const Chat = () => {
               <React.Fragment key={i}>
                 {i % 2 === 0 ? (
                   <div className="flex items-end justify-start">
-                    <div className="">
-                      <img src={icon} alt="logo" className="mx-auto" />
-                      <p>{child[i]?.speaker}</p>
+                    <div className="w-24 text-center">
+                      <ListSvg
+                        fill="#FF0000"
+                        style={{ margin: "auto" }}
+                        color={"blue"}
+                      />
+                      <p className="pt-1 text-sm md:text-lg">
+                        {origin[i]?.speaker}
+                      </p>
                     </div>
                     <BubbleRight content={formattedSpeech} />
                   </div>
                 ) : (
                   <div className="flex items-end justify-end">
                     <BubbleLeft content={formattedSpeech} />
-                    <div className="text-center">
-                      <img src={icon} alt="logo" className="mx-auto" />
-                      <p>{child[i]?.speaker}</p>
+                    <div className="w-24 text-center">
+                      <ListSvg
+                        fill="#FF0000"
+                        style={{ margin: "auto" }}
+                        color={"blue"}
+                      />
+                      <p className="pt-1 text-sm md:text-lg">
+                        {origin[i]?.speaker}
+                      </p>
                     </div>
                   </div>
                 )}
@@ -188,16 +224,16 @@ export const Chat = () => {
   };
 
   return (
-    <section className="fixed left-0 top-[10%] h-full w-full overflow-y-scroll">
+    <section className=" left-0 h-full pt-2">
       <div
         role="tablist"
-        className="tabs-boxed tabs fixed right-10 top-20 flex h-[5%] items-center justify-end space-x-2 bg-main-color"
+        className="tabs-boxed tabs fixed right-1 top-0 z-50 flex h-[5%] items-center justify-end space-x-2 bg-white"
       >
         <a
           role="tab"
           className={`tab ${activeTab === 1 ? "tab-active" : ""} text-black`}
           onClick={() => handleTabClick(1)}
-          style={{ backgroundColor: activeTab === 1 ? "white" : "" }}
+          style={{ backgroundColor: activeTab === 1 ? "RGB(162,176,199)" : "" }}
         >
           原文
         </a>
@@ -205,7 +241,7 @@ export const Chat = () => {
           role="tab"
           className={`tab ${activeTab === 2 ? "tab-active" : ""} text-black`}
           onClick={() => handleTabClick(2)}
-          style={{ backgroundColor: activeTab === 2 ? "white" : "" }}
+          style={{ backgroundColor: activeTab === 2 ? "RGB(162,176,199)" : "" }}
         >
           要約
         </a>
@@ -213,7 +249,7 @@ export const Chat = () => {
           role="tab"
           className={`tab ${activeTab === 3 ? "tab-active" : ""} text-black`}
           onClick={() => handleTabClick(3)}
-          style={{ backgroundColor: activeTab === 3 ? "white" : "" }}
+          style={{ backgroundColor: activeTab === 3 ? "RGB(162,176,199)" : "" }}
         >
           簡易
         </a>
