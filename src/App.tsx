f9c07901f47eae53ccd2b7bccb691c62ca632de2
shimizu <noon3m.op.sh@gmail.com>
2024-02-01 11:22:48 +0900

    [fix]headerの重複修正

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -18,7 +17,6 @@ function App() {
         >
           Learn React
         </a>
-      </header>
     </div>
   );
 }
