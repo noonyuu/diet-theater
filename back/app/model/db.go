fa3750ccc32ecbfafae6b6770dd2cd97b4d85ac9
shimizu <noon3m.op.sh@gmail.com>
2024-04-23 00:01:36 +0900

    [update]パスワードを環境変数に

--- back/app/model/db.go
+++ back/app/model/db.go
@@ -1,15 +1,27 @@
 package model
 
 import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 var db *gorm.DB
 
+func loadEnv() {
+	err := godotenv.Load("../../env")
+	if err != nil {
+		log.Panic("Error loading .env file")
+	}
+}
+
 func OpenDB() {
+	loadEnv()
 	var err error
-	dsn := "root:root@tcp(db_diet:3306)/diet?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "diet:" + os.Getenv("DB_PASS")+"@tcp(db_diet:3306)/diet?charset=utf8mb4&parseTime=True&loc=Local"
 
 	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
@@ -27,7 +39,7 @@ func DeleteDB() {
 }
 
 func NewDBConnection() (*gorm.DB, error) {
-	dsn := "root:root@tcp(db_diet:3306)/diet?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "diet:diet@tcp(db_diet:3306)/diet?charset=utf8mb4&parseTime=True&loc=Local"
 	var err error
 	db, err = gorm.Open(mysql.New(mysql.Config{
 		DSN: dsn,
