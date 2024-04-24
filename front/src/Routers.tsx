73a21d65100aa07f587dc513842bc6e3f3dc9634
shimizu <noon3m.op.sh@gmail.com>
2024-04-24 16:21:00 +0900

    [fix]デプロイ時にログインの処理がバグってるので退避

--- front/src/Routers.tsx
+++ front/src/Routers.tsx
@@ -7,40 +7,41 @@ import Login from "./pages/Login";
 // components
 import NotFound from "./component/notFound";
 import { GetUser, RefreshToken } from "./script/Auth";
+import { Terms } from "./pages/Terms";
 
 const Routers = () => {
   const navigate = useNavigate();
 
-  useEffect(() => {
-    const fetchUser = async () => {
-      try {
-        const [logInned, userInfo] = await GetUser();
-        if (logInned && userInfo) {
-          // トークン更新
-          RefreshToken();
-          // ログイン済みの場合
-          if (
-            window.location.pathname === "/" ||
-            window.location.pathname === "/login-test"
-          ) {
-            navigate("/secret/agenda");
-          } else {
-            navigate(window.location.pathname, { replace: true });
-          }
-        } else {
-          navigate("/login-test");
-        }
-      } catch (e) {
-        console.log(e);
-      }
-    };
-    fetchUser();
-  }, []);
+  // useEffect(() => {
+  //   const fetchUser = async () => {
+  //     try {
+  //       const [logInned, userInfo] = await GetUser();
+  //       if (logInned && userInfo) {
+  //         // トークン更新
+  //         RefreshToken();
+  //         // ログイン済みの場合
+  //         if (
+  //           window.location.pathname === "/" ||
+  //           window.location.pathname === "/login-test"
+  //         ) {
+  //           navigate("/secret/agenda");
+  //         } else {
+  //           navigate(window.location.pathname, { replace: true });
+  //         }
+  //       } else {
+  //         navigate("/login-test");
+  //       }
+  //     } catch (e) {
+  //       console.log(e);
+  //     }
+  //   };
+  //   fetchUser();
+  // }, []);
 
   return (
     <>
       <Routes>
-        <Route path="/" element={<Login />}></Route>
+        <Route path="/" element={<Terms />}></Route>
         <Route path="secret/*" element={<Secret />} />
         <Route path="/*" element={<NotFound />} />
 
