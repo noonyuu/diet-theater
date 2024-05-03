4a7e3bfe470c2e8606fffb7c1eee22405a3ed55d
shimizu <noon3m.op.sh@gmail.com>
2024-05-03 23:43:04 +0900

    [update]ルーティングの仕様を変更

--- front/src/Routers.tsx
+++ front/src/Routers.tsx
@@ -6,8 +6,20 @@ import Secret from "./secret/Secret";
 import Login from "./pages/Login";
 // components
 import NotFound from "./component/notFound";
-import { GetUser, RefreshToken } from "./script/Auth";
+// import { GetUser, RefreshToken } from "./script/Auth";
 import { Terms } from "./pages/Terms";
+import Footer from "./component/Footer";
+import Header from "./component/Header";
+import { ContextWrapper } from "./context/ContextWrapper";
+import { AdminAgenda } from "./pages/admin/AdminAgenda";
+import CreateCard from "./pages/admin/CreateCard";
+import StandbyScreen from "./pages/admin/StandbyScreen";
+import { LoginPage } from "./pages/admin/login";
+import ExcelReader from "./pages/admin/xlsx";
+import Test from "./pages/user/Test";
+import Article from "./pages/user/article/Article";
+import { Agenda } from "./pages/user/summary/Agenda";
+import { Chat } from "./pages/user/summary/Chat";
 
 const Routers = () => {
   const navigate = useNavigate();
@@ -42,12 +54,29 @@ const Routers = () => {
     <>
       <Routes>
         <Route path="/" element={<Terms />}></Route>
-        <Route path="secret/*" element={<Secret />} />
-        <Route path="/*" element={<NotFound />} />
+        <Route path="/home" element={<Secret />} />
+        {/* <Route path="/*" element={<NotFound />} /> */}
 
         {/* test */}
         <Route path="/login-test" element={<Login />} />
       </Routes>
+      
+      <ContextWrapper>
+        <Header />
+        <Routes>
+          <Route path="/admin-agenda" element={<AdminAgenda />} />
+          <Route path="/standby-screen/:issueID" element={<StandbyScreen />} />
+          <Route path="/crate-card" element={<CreateCard />} />
+          <Route path="/x" element={<ExcelReader />} />
+          <Route path="/login" element={<LoginPage />}></Route>
+          <Route path="/terms" element={<Terms />} />
+          <Route path="/agenda" element={<Agenda />} />
+          <Route path="/chat" element={<Chat />} />
+          <Route path="/article" element={<Article />} />
+          <Route path="/test" element={<Test />} />
+        </Routes>
+        <Footer />
+      </ContextWrapper>
     </>
   );
 };
