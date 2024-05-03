437eae769dd92830c9bb181dc82667a73c3832af
shimizu <noon3m.op.sh@gmail.com>
2024-05-04 01:44:00 +0900

    [update]修正

--- front/src/component/Search.tsx
+++ front/src/component/Search.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 
-const Search = () => {
+// props の型を定義
+interface SearchProps {
+  placeholder: string;
+}
+
+const Search: React.FC<SearchProps> = ({ placeholder }) => {
   return (
-    <>
-      <input
-        type="text"
-        name=""
-        id=""
-        className="rounded-l-lg rounded-r-none border-2 bg-white"
-      />
-    </>
+    <input
+      type="search"
+      placeholder={placeholder}
+      className="w-full rounded-md border-[1px] border-gray-200 bg-white p-1 focus:border-green-200 focus:outline-none"
+    />
   );
 };
 
