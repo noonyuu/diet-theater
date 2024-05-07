e07ebcdc0a6f36520cc1fd6781fdaf455d16cab7
shimizu <noon3m.op.sh@gmail.com>
2024-05-08 00:17:36 +0900

    [update]構造体を修正

--- back/app/database/structs.go
+++ back/app/database/structs.go
@@ -9,33 +9,26 @@ import (
 type MeetingRecord struct {
 	ID            uint           `gorm:"primaryKey"`                        // 複合主キーの一部としてIDを定義
 	IssueID       string         `gorm:"primaryKey;type:char(21);not null"` // 複合主キーの一部としてIssueIDを定義
-	Session       int            `gorm:"not null"`
-	NameOfHouse   string         `gorm:"type:varchar(5);not null"`
-	NameOfMeeting string         `gorm:"type:text"`
-	Issue         string         `gorm:"type:varchar(5)"`
-	Date          string         `gorm:"type:date"`
+	Session       int            `gorm:"not null"`                          // 国会回次
+	NameOfHouse   string         `gorm:"type:varchar(5);not null"`          // 院名
+	NameOfMeeting string         `gorm:"type:text"`                         // 会議名
+	Issue         string         `gorm:"type:varchar(5)"`                   // 号数
+	Date          string         `gorm:"type:date"`                         // 開催日
 	CreatedAt     time.Time      // gorm.Modelから分離
 	UpdatedAt     time.Time      // gorm.Modelから分離
 	DeletedAt     gorm.DeletedAt `gorm:"index"` // 論理削除をサポート
 }
 
-type RecordTag struct {
-	IssueID string `json:"issue_id" gorm:"primaryKey;type:char(4);not null"` // 複合主キーの一部としてIssueIDを定義
-	TagID   string `json:"tag_id" gorm:"primaryKey;type:char(4);not null"`   // 複合主キーの一部としてTagを定義
-}
-
-type Tag struct {
-	TagID   string `gorm:"primaryKey;type:char(4);not null"`
-	TagName string `gorm:"type:varchar(32);not null"`
-}
-
 type SpeechRecord struct {
-	ID            uint   `gorm:"primaryKey"`                        // 複合主キーの一部としてIDを定義
-	IssueID       string `gorm:"primaryKey;type:char(21);not null"` // 複合主キーの一部としてIssueIDを定義
-	SpeechID      string `gorm:"primaryKey;type:int;not null"`      // 複合主キーの一部としてSpeechIDを定義
-	Speaker       string `gorm:"type:varchar(16);not null"`
-	SpeakerYomi   string `gorm:"type:varchar(32)"`
-	SpeakerGroup  string `gorm:"type:varchar(32)"`
-	SpeechOrigin  string `gorm:"type:text;not null"`
-	SpeechSummary string `gorm:"type:text;not null"`
+	ID              uint   `gorm:"primaryKey"`                        // 複合主キーの一部としてIDを定義
+	IssueID         string `gorm:"primaryKey;type:char(21);not null"` // 複合主キーの一部としてIssueIDを定義
+	SpeechID        string `gorm:"primaryKey;type:int;not null"`      // 複合主キーの一部としてSpeechIDを定義
+	Speaker         string `gorm:"type:varchar(16);not null"`         // 発言者
+	SpeakerYomi     string `gorm:"type:varchar(32)"`                  // 読み仮名
+	SpeakerRole     string `gorm:"type:varchar(32)"`                  // 役割
+	SpeakerGroup    string `gorm:"type:varchar(32)"`                  // 会派
+	SpeakerPosition string `gorm:"type:varchar(32)"`                  // 肩書き
+	SpeechOrigin    string `gorm:"type:text;not null"`                // 原文
+	SpeechSummary   string `gorm:"type:text;not null"`                // 要約
+	AnimationPoint  string `gorm:"type:varchar(5)"`                   // アニメーションポイント
 }
