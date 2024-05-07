4431fea0f2fbd99bd2548f0480679a27ca7d8f54
shimizu <noon3m.op.sh@gmail.com>
2024-05-08 00:04:11 +0900

    [update]データベース挿入用のエンドポイント

--- back/app/gin_main/main.go
+++ back/app/gin_main/main.go
@@ -2,11 +2,19 @@ package main
 
 import (
 	"api/database"
-	"api/middleware"
-	"fmt"
-	"log"
+	// "io"
+	"net/http"
 
 	// "net/http"
+
+	// "net/http"
+
+	// "api/handler"
+	"api/middleware"
+	// "api/model"
+
+	"fmt"
+	"log"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
@@ -20,20 +28,28 @@ func loadEnv() {
 	}
 }
 
-type BookData struct {
-	Name string
-	ID   string
+type IssueID struct {
+	IssueID string `json:"issueID"`
 }
 
-type IssueID struct {
-	ID string
+func hello(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "Hello, World!",
+	})
 }
 
 func main() {
 	loadEnv()
 	middleware.Init()
 	GeminiInit()
-	database.OpenDB()
+
+	err := database.Init()
+
+	//エラー処理
+	if err != nil {
+		//パニックを起こす
+		log.Fatalln(err)
+	}
 
 	server := gin.Default()
 
@@ -46,28 +62,82 @@ func main() {
 	})
 
 	// geminiに問い合わせ
-	server.POST("/summary", func(c *gin.Context) {
-		fmt.Println("summary post")
-		var issue IssueID
-		if err := c.ShouldBindJSON(&issue); err != nil {
-			log.Println(err)
-			//エラーを返す
-			c.JSON(500, gin.H{"message": err.Error()})
+	// server.POST("/summary", func(c *gin.Context) {
+	// 	fmt.Println("summary post")
+	// 	var issue IssueID
+	// 	if err := c.ShouldBindJSON(&issue); err != nil {
+	// 		log.Println(err)
+	// 		//エラーを返す
+	// 		c.JSON(500, gin.H{"message": err.Error()})
+	// 		return
+	// 	}
+
+	// 	result, err := CallAI(issue.IssueID)
+	// 	if err != nil {
+	// 		log.Println(err)
+	// 		c.JSON(500, gin.H{"message": err.Error()})
+	// 		return
+	// 	}
+	// 	fmt.Println(issue)
+	// 	c.JSON(200, gin.H{
+	// 		"message": result,
+	// 	})
+	// })
+
+	server.POST("/speech_record/insert", func(c *gin.Context) {
+		fmt.Println("speech_record insert")
+		var speechRecords []*database.SpeechRecord
+		if err := c.ShouldBindJSON(&speechRecords); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 			return
 		}
 
-		result, err := CallAI("1", issue.ID)
+		dbConn, err := database.GetDB()
 		if err != nil {
-			log.Println(err)
-			c.JSON(500, gin.H{"message": err.Error()})
+			// データベース接続の取得に失敗した場合のエラーハンドリング
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to connect to the database."})
 			return
 		}
-		fmt.Println(issue)
-		c.JSON(200, gin.H{
-			"message": result,
-		})
+		if dbConn == nil {
+			// dbConnがnilの場合のエラーハンドリング
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Database connection is nil."})
+			return
+		}
+
+		tx := dbConn.Begin() // トランザクションの開始
+		for _, speechRecord := range speechRecords {
+			if err := tx.Create(&speechRecord).Error; err != nil {
+				tx.Rollback() // エラーが発生した場合はロールバック
+				c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+				return
+			}
+		}
+		tx.Commit() // トランザクションのコミット
+		c.JSON(http.StatusCreated, speechRecords)
 	})
 
+	/**
+	 * 以下はテスト用のエンドポイント
+	 */
+	//  接続テスト用のエンドポイント
+	// 	server.POST("/speech_record/insert", func(c *gin.Context) {
+	// 	fmt.Println("speech_record insert")
+
+	// 	// リクエストボディを読み取る
+	// 	bodyBytes, err := io.ReadAll(c.Request.Body)
+	// 	if err != nil {
+	// 		// エラーがあれば、クライアントにエラーを返す
+	// 		c.JSON(500, gin.H{"error": "リクエストボディの読み取りに失敗しました"})
+	// 		return
+	// 	}
+
+	// 	// 読み取ったボディを文字列として出力する
+	// 	bodyString := string(bodyBytes)
+	// 	fmt.Println(bodyString)
+
+	// 	// 処理が成功した場合、適切なレスポンスを返す
+	// 	c.JSON(200, gin.H{"message": "正常に処理されました"})
+	// })
 	// SpeakerText 構造体を定義
 	type SpeakerText struct {
 		Speaker string `json:"speaker"`
@@ -75,19 +145,7 @@ func main() {
 	}
 
 	server.GET("/test", func(c *gin.Context) {
-		// test := map[int]SpeakerText{
-		// 	0: {Speaker: "石田博英", Speech: "石田議長が本両院協議会の議長に就任。各議院の議長および副議長が発表された。本案は傍聴が認められないため、関係者以外の退室が求められた。協議に入る前に、衆議院から両院協議会を求めた理由と衆議院側の趣旨の説明が行われる。"},
-		// 	1: {Speaker: "西村直己", Speech: "衆議院から修正案が提出された。主な内容は以下の通り。\n\n* 「とうもろこし」など7品目を1年間免税\n* ガソリン・潤滑油を10%減税\n* 建染染料（人造藍を除く）の従価税率を1年間15%に明確化（政府案では「当分の間」15%）"},
-		// 	2: {Speaker: "石田博英", Speech: "石田博英議長が、参議院側の議決の趣旨を説明し、要点は次の通り。\n\n* 法案を可決する。\n* 国民の生命、健康を守るために緊急避難的な措置が必要と判断。\n* 医療体制の強化に万全を期す。\n* 国民生活への影響を最小限に抑える努力をする。"},
-		// 	3: {Speaker: "小林政夫", Speech: "小林政夫は、衆議院案に一部異議を唱えた。建染染料の関税率は25％とするべきで、飼料の「こうりやん」と油糧種の菜種・からし菜は1年間免税とし、産業近代化のための輸入機械も1年間免税とするよう主張した。この追加修正は工業保護と国民生活への配慮によるものである。"},
-		// 	4: {Speaker: "石田博英", Speech: "石田議長のもと、両院協議会を開催。議決の趣旨に関する説明が行われた後、質疑が行われなかったため、懇談に移って協議が開始された。"},
-		// 	5: {Speaker: "石田博英", Speech: "石田博英議長のもと、懇談会が開催された。懇談会は午後8時46分から5分間行われた。"},
-		// 	6: {Speaker: "石田博英", Speech: "石田議長が衆議院議決案を修正。改正後の関税定率法一部改正案は、協議会案として審議する決定が下された。具体的には、附則第一項の施行日を5月1日に変更し、別表乙号705（合成染料・建染染料）の税率を2%に改定する。"},
-		// 	7: {Speaker: "石田博英", Speech: "石田議長は協議案の採決を行い、総員起立により全員が賛成した。"},
-		// 	8: {Speaker: "石田博英", Speech: "議長は、協議会案が両院協議会の成案になったことを発表。案文整理を議長に委任し、異議がないことを確認した。"},
-		// 	9: {Speaker: "石田博英", Speech: "両院協議会の議長・石田博英が散会を宣言。協議会の任務が終了したことを確認し、午後8時53分に散会。"},
-		// }
-		test:= []SpeakerText{
+		test := []SpeakerText{
 			{Speaker: "石田博英", Speech: "石田議長が本両院協議会の議長に就任。各議院の議長および副議長が発表された。本案は傍聴が認められないため、関係者以外の退室が求められた。協議に入る前に、衆議院から両院協議会を求めた理由と衆議院側の趣旨の説明が行われる。"},
 			{Speaker: "西村直己", Speech: "衆議院から修正案が提出された。主な内容は以下の通り。\n\n* 「とうもろこし」など7品目を1年間免税\n* ガソリン・潤滑油を10%減税\n* 建染染料（人造藍を除く）の従価税率を1年間15%に明確化（政府案では「当分の間」15%）"},
 			{Speaker: "石田博英", Speech: "石田博英議長が、参議院側の議決の趣旨を説明し、要点は次の通り。\n\n* 法案を可決する。\n* 国民の生命、健康を守るために緊急避難的な措置が必要と判断。\n* 医療体制の強化に万全を期す。\n* 国民生活への影響を最小限に抑える努力をする。"},
