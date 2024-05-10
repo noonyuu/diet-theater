18ba501cc7d8ab81705853c73d8740f9f0717acc
shimizu <noon3m.op.sh@gmail.com>
2024-05-10 13:26:33 +0900

    [add]矢印キーを押した時にも動くように

--- front/src/pages/user/Theater.tsx
+++ front/src/pages/user/Theater.tsx
@@ -1,11 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import "./theater.css";
 import axios from "axios";
+
 // TODO:リロード時に続きから表示するかの選択
 var path = import.meta.env.VITE_APP_PATH;
 export const Theater = () => {
   const [speechRecords, setSpeechRecords] = useState<any[]>([]); //  スピーチレコード
   const [currSpeechRecord, setCurrSpeechRecord] = useState<number>(0); //  現在のスピーチレコード
+
+  const [isFirstLoad, setIsFirstLoad] = useState<boolean>(false); //  初回ロード
 
   useEffect(() => {
     document.body.classList.add("no-scroll");
@@ -20,35 +23,60 @@ export const Theater = () => {
       try {
         const response = await axios.get(
           "https://" + path + "/app/speech_record/select/all",
+          // "https://yeeeee-waaaaaa.noonyuu.com/app/speech_record/select/all",
         );
 
         if (Array.isArray(response.data)) {
           setSpeechRecords(response.data);
+          setIsFirstLoad(true);
         } else {
           console.error("Unexpected response structure:", response.data);
         }
       } catch (error) {
         console.error("Error fetching data:", error);
-      }
+      } 
     };
 
     fetchData();
   }, []);
 
+  // キーボード操作
+  const handleKeyUp = useCallback(
+    (e: { keyCode: any }) => {
+      const keyCode = e.keyCode;
+
+      if (keyCode === 37) {
+        back();
+      }
+      if (keyCode === 39) {
+        next();
+      }
+    },
+    [isFirstLoad],
+  );
+
+  useEffect(() => {
+    if (isFirstLoad) {
+      addEventListener("keyup", (e) => handleKeyUp(e));
+    }
+  }, [isFirstLoad]);
+
   // 次のスピーチレコード
   const next = () => {
-    if (currSpeechRecord === speechRecords.length - 1) {
-      return;
-    }
-    setCurrSpeechRecord(currSpeechRecord + 1);
+    setCurrSpeechRecord((curr) => {
+      // console.log("next length", speechRecords);
+      return curr === speechRecords.length - 1 ? curr : curr + 1;
+    });
   };
+
   // 前のスピーチレコード
   const back = () => {
-    if (currSpeechRecord === 0) {
-      return;
-    }
-    setCurrSpeechRecord(currSpeechRecord - 1);
+    setCurrSpeechRecord((curr) => {
+      return curr === 0 ? curr : curr - 1;
+    });
   };
+
+  const finish = () => {};
 
   return (
     <main className="theater-back relative h-full">
@@ -61,7 +89,7 @@ export const Theater = () => {
         &lt;
       </button>
       <div className="balloon1 absolute left-[50%] top-24 -translate-x-1/2 rounded-md p-24 text-2xl">
-        {speechRecords.length > 0 && (
+        {speechRecords.length > 0 && speechRecords[currSpeechRecord] && (
           <div className="w-full text-center">
             {speechRecords[currSpeechRecord].SpeechSummary.replaceAll(
               "「",
