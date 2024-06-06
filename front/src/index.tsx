7dcf886a12ca5fad2621dae36e480316cbd4bf91
shimizu <noon3m.op.sh@gmail.com>
2024-06-06 16:38:26 +0900

    chore: Update npm dependencies and add react-icons package

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,25 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
-<<<<<<< HEAD:src/index.tsx
-=======
-import App from './App';
->>>>>>> develop:front/src/index.tsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import App from "./App";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement,
 );
 root.render(
-<<<<<<< HEAD:src/index.tsx
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-=======
   // <React.StrictMode>
-    <App />
+  <App />,
   // </React.StrictMode>
->>>>>>> develop:front/src/index.tsx
 );
 
 // If you want to start measuring performance in your app, pass a function
