ad996b65762afb8475fcfc60224bebf03172083c
shimizu <noon3m.op.sh@gmail.com>
2024-09-26 21:17:47 +0900

    [change] 構造体修正と関連するリポジトリのインターフェースを修正

---
+++ back/diet_api/domain/repositories/meetiong_record_repository.go
@@ -0,0 +1,12 @@
+package repositories
+
+import (
+	"context"
+	"diet-theater/back/diet_api/domain/entities"
+)
+
+type IMeetingRecordRepository interface {
+	GetMeetingRecordOnce(ctx context.Context, issueID string) (*entities.MeetingRecord, error)
+	GetMeetingRecordAll(ctx context.Context) ([]*entities.MeetingRecord, error)
+	CreateMeetingRecord(ctx context.Context, meetingRecord *entities.MeetingRecord) (*entities.MeetingRecord, error) // 戻り値を修正
+}
--- back/diet_api/domain/repositories/view_history_repository.go
+++ back/diet_api/domain/repositories/view_history_repository.go
@@ -6,6 +6,6 @@ import (
 )
 
 type IViewHistoryRepository interface {
-	GetViewHistory(ctx context.Context) (*entities.ViewHistory, error)
+	GetViewHistory(ctx context.Context, user_id string) (*entities.ViewHistory, error)
 	CreateViewHistory(ctx context.Context, viewHistory *entities.ViewHistory) (uint, error)
 }
