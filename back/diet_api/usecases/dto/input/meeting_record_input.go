b758f35c9ddd89529971c55e0636fcf21cbe846f
shimizu <noon3m.op.sh@gmail.com>
2024-09-29 23:46:29 +0900

    [add] meeting_recordのdto 入力用構造体作成

---
+++ back/diet_api/usecases/dto/input/meeting_record_input.go
@@ -0,0 +1,11 @@
+package input
+
+type CreateMeetingRecordInput struct {
+	MeetingRecordID uint
+	IssueID         string
+	Session         int
+	NameOfHouse     string
+	NameOfMeeting   string
+	Issue           string
+	Date            string
+}
