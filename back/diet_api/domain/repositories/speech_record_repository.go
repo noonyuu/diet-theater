5fe02558fe7b6a3956ebc902550c63626576d472
shimizu <noon3m.op.sh@gmail.com>
2024-10-03 13:10:14 +0900

    [change]createの受け取り値と返り値を配列に対応させた

--- back/diet_api/domain/repositories/speech_record_repository.go
+++ back/diet_api/domain/repositories/speech_record_repository.go
@@ -9,5 +9,5 @@ type ISpeechRecordRepository interface {
 	GetSpeechRecordOnce(ctx context.Context, issue_id uint) (*entities.SpeechRecord, error)
 	GetSpeechRecordOnceBySpeechID(ctx context.Context, issue_id uint, speech_id uint) (*entities.SpeechRecord, error)
 	GetSpeechRecordAll(ctx context.Context) ([]*entities.SpeechRecord, error)
-	CreateSpeechRecord(ctx context.Context, speechRecord *entities.SpeechRecord) (uint, error)
+	CreateSpeechRecord(ctx context.Context, speechRecord []*entities.SpeechRecord) ([]*entities.SpeechRecord, error)
 }
