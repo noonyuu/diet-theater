e88c324b90a04efe8afe56b9c29360502071ed8c
shimizu <noon3m.op.sh@gmail.com>
2024-10-06 22:51:26 +0900

    [change]interfaceの定義をtypesフォルダに切り分けた

---
+++ front/src/types/speech.ts
@@ -0,0 +1,13 @@
+export interface Speech {
+  ID: number;
+  IssueID: string;
+  SpeechID: string;
+  Speaker: string;
+  SpeakerGroup: string;
+  SpeakerPosition: string;
+  SpeakerRole: string;
+  SpeakerYomi: string;
+  SpeakerOrigin: string;
+  SpeakerSummary: string;
+  AnimationPoint: string;
+}
